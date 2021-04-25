115cf01b96206b9c1dd201358a4d943ebbc9c894
Anselmo Jacytnho <anselmojacyntho@gmail.com>
2021-04-25 14:18:45 -0400

    fix: add date sale in order table

--- app/database/migrations/2021_04_25_161724_create_orders_table.php
+++ app/database/migrations/2021_04_25_161724_create_orders_table.php
@@ -16,6 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->float('total');
+            $table->date('date_sale');
             $table->timestamps();
         });
     }
