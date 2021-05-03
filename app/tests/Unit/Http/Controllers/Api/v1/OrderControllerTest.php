0a659be53314025a8d0553539ca0af1a54bcc3a7
Nícolas Costa <nicolas.nico@hotmail.com>
2021-05-03 04:38:03 -0300

    test: add OrderControllerTest

---
+++ app/tests/Unit/Http/Controllers/Api/v1/OrderControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\Api\v1;
+
+use App\Models\DeliveryAddress;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Supplier;
+use Tests\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_if_it_returns_the_order_data()
+    {
+        $products = Product::factory()
+            ->for(Supplier::factory())
+            ->count(2);
+
+        $deliveryAddress = DeliveryAddress::factory();
+
+        $order = Order::factory()
+            ->has($products)
+            ->has($deliveryAddress)
+            ->create();
+
+        $response = $this->get('/api/v1/order/'.$order->id);
+
+        $response->assertJsonFragment([
+            'id' => $order->id
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
