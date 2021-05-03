6536eb415dabd97cb3111713bc2e3d4216ee1059
Nícolas Costa <nicolas.nico@hotmail.com>
2021-05-03 09:32:45 -0300

    feature: add fillable, relation and softdelete trait to DeliveryAddress model

--- app/app/Models/DeliveryAddress.php
+++ app/app/Models/DeliveryAddress.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DeliveryAddress extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'address',
+        'number',
+        'zip',
+        'city_id'
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
